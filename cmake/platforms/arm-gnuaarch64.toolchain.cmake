edc1cc012494afca8d088fe8fda86fa5bd3230cf
Mickael Gaillard <mick.gaillard@gmail.com>
2020-11-22 17:57:35 +0100

    fix multi-ARM platform
    Signed-off-by: Mickael Gaillard <mick.gaillard@gmail.com>

--- cmake/platforms/arm-gnuaarch64.toolchain.cmake
+++ cmake/platforms/arm-gnuaarch64.toolchain.cmake
@@ -5,11 +5,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-set(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabi-g++)
+set(CMAKE_C_COMPILER   /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
 # where is the target environment 
-set(CMAKE_FIND_ROOT_PATH  /usr/arm-linux-gnueabi)
+set(CMAKE_FIND_ROOT_PATH  /usr/aarch64-linux-gnu)
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -19,5 +19,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 
 set(OW_BOARD "raspberry")
-set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "armel")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "arm64")
 
